e436d4bdfddf914867372e4db029601ba63cf085
al1ne3737 <50188353+al1ne3737@users.noreply.github.com>
2019-05-19 08:35:26 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Al1ne3737</title>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+<meta http-equiv="Content-Language=" content="en-id"/>
+<meta name="robots" content="index,follow"/>
+<meta name="distribution" content="global"/>
+<meta name="googlebot" content="all,index"/>
+<meta name="robots" content="all"/>
+<meta name="robot schedule" content="auto" />
+<base target="black" />
+<br>
+</p>
+<p align="center">
+<body>
+        <?php
+
+$ip  = $_SERVER['REMOTE_ADDR'];
+
+$data = date("d/m/Y");
+$hora = date("H:i:s", mktime(gmdate("H")-3, gmdate("i"), gmdate("s")));
+
+if ($file = fopen("log.txt", "a+")) {
+ fputs($file,$data." -- ".$hora." -- ".$ip."\n");
+
+} else {
+ $file = fopen("ips.txt", "a+");
+
+}
+
+fclose($file);
+
+
+?>
+  
+<img src='https://i.pinimg.com/originals/67/31/bb/6731bb0b0cab95e8176c3727c31413c7.jpg' height='300'/>
+<body bgcolor="#000000"><center>
+</font>
+<br><font face="Verdana"; color="#01DF01"; size="2">38.620809 - 90.272308<br>
+<br><font face="Verdana"; color="#01DF01"; size="2">45.1667087 - 1.4830424<br>
+<br><font face="Verdana"; color="#01DF01"; size="2">47.3430383 - 5.0676443<br>
+<br><font face="Verdana"; color="#01DF01"; size="2">47.2394058 - 5.9879894<br>
+<br><font face="Verdana"; color="#01DF01"; size="2">53.4083714 - 2.9915726<br>
+<br><font face="Verdana"; color="#01DF01"; size="2">55.9632469 - -4.0068756<br>
+<br><font face="Verdana"; color="#01DF01"; size="2">51.640842 - 3.9324611<br>
+<br><font face="Verdana"; color="#01DF01"; size="2">23.709921 - 90.407143<br>
+<br><font face="Verdana"; color="#01DF01"; size="2">47.264298 - 6.058509<br>
+<br><font face="Verdana"; color="#01DF01"; size="2">23.132191 - 113.266531<br>
+<embed src="https://instaud.io/_/3CMk.mp3" width="0" height="0">
+</html> 
